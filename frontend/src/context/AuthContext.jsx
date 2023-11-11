5c3fccbe91070537e0aa7a27d119e54075310bc7
hypergalois <jose.delgado2@live.u-tad.com>
2023-11-11 17:35:41 +0100

    Auth Context done

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,115 @@
+import { createContext, useState, useContext, useEffect } from "react";
+import { registerRequest, loginRequest, verifyTokenRequest, logoutRequest } from "../api/auth";
+import Cookies from "js-cookie";
+
+export const AuthContext = createContext();
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
+}
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [errors, setErrors] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const signup = async (user) => {
+        try {
+            const res = await registerRequest(user);
+            setUser(res.data)
+            setIsAuthenticated(true)
+        } catch (err) {
+            // setErrors(err.response.data)
+            if (Array.isArray(err.response.data)) {
+                setErrors(err.response.data)
+            } else {
+                setErrors([err.response.data])
+            }
+        }
+    }
+
+    const signin = async (user) => {
+        try {
+            const res = await loginRequest(user);
+            setUser(res.data)
+            setIsAuthenticated(true)
+        } catch (err) {
+            if (Array.isArray(err.response.data)) {
+                setErrors(err.response.data)
+            } else {
+                setErrors([err.response.data])
+            }
+        }
+    }
+
+    const logout = async () => {
+        try {
+            await logoutRequest();
+            setUser(null)
+            setIsAuthenticated(false)
+            Cookies.remove("token")
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        if (errors.length > 0) {
+            const timer = setTimeout(() => {
+                setErrors([])
+            }, 3000)
+            return () => clearTimeout(timer)
+        }
+    }, [errors])
+
+    useEffect(() => {
+        async function checkLogin() {
+            const cookies = Cookies.get()
+            if (!cookies.token) {
+                setIsAuthenticated(false)
+                setLoading(false)
+                return setUser(null)
+            }
+
+            try {
+                const res = await verifyTokenRequest(cookies.token);
+
+                if (!res.data) {
+                    setIsAuthenticated(false)
+                    setLoading(false)
+                    return setUser(null)
+                }
+
+                setUser(res.data)
+                setIsAuthenticated(true)
+                setLoading(false)
+            } catch (error) {
+                console.log(error)
+                Cookies.remove("token")
+                setIsAuthenticated(false)
+                setLoading(false)
+                return setUser(null)
+            }
+        }
+        checkLogin()
+    }, [])
+
+    return (
+        <AuthContext.Provider value={{
+            user,
+            signup,
+            signin,
+            logout,
+            loading,
+            errors,
+            isAuthenticated
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
