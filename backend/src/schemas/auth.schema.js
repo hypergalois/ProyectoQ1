cf3d270d39d75a62e8fcba565847a59aaac48a97
hypergalois <jose.delgado2@live.u-tad.com>
2023-11-11 15:15:50 +0100

    Schemas added for auth and recipe

--- backend/src/schemas/auth.schema.js
+++ backend/src/schemas/auth.schema.js
@@ -9,5 +9,20 @@ export const registerSchema = z.object({
     }),
     password: z.string({
         required_error: 'La contraseña es requerida'
-    }).min(6, 'La contraseña debe tener al menos 6 caracteres'),
+    }).min(6, {
+        message: 'La contraseña debe tener al menos 6 caracteres'
+    })
+})
+
+export const loginSchema = z.object({
+    email: z.string({
+        required_error: 'El correo electrónico es requerido'
+    }).email({
+        message: "El correo electrónico no es válido"
+    }),
+    password: z.string({
+        required_error: 'La contraseña es requerida'
+    }).min(6, {
+        message: 'La contraseña debe tener al menos 6 caracteres'
+    })
 })
---
+++ backend/src/schemas/recipe.schema.js
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+export const createRecipeSchema = z.object({
+    title: z.string({
+        required_error: "El título es requerido"
+    }),
+    description: z.string({
+        required_error: "La descripción es requerida"
+    }).optional(),
+    date: z.string().datetime().optional()
+})
+
+export const updateTaskSchema = z.object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+    date: z.string().datetime().optional()
+})
