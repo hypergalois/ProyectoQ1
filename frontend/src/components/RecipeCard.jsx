bd8d2fd477324a80521df067e609d680bfc39acb
AnderAguinagaUtad <anderaginaga3@gmail.com>
2023-11-16 00:15:13 +0100

    Translate delete and edit buttons

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -15,10 +15,10 @@ function RecipeCard({ recipe }) {
                     <button onClick={() => {
                         deleteRecipe(recipe._id);
                     }} className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md">
-                        delete
+                        Eliminar
                     </button>
                     <Link to={`/recipes/${recipe._id}`} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md">
-                        edit
+                        Editar
                     </Link>
                 </div>
             </header>
